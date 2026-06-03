55de915459914dc6903f138813ed62de09872910
Damián <srcarlosdamian@gmail.com>
2026-06-03 00:21:07 -0600

    Add files via upload

---
+++ Frame/Frame.tsx
@@ -0,0 +1,104 @@
+import svgPaths from "./svg-gvh07irm3e";
+import imgImage38 from "./2f36c56ffd2582433f79606ba772d68e9e9477f2.png";
+import imgImage39 from "./dfcdaa12c7385a5feb1cee38f25a8762e46c2e69.png";
+import imgImage40 from "./37e22366911c39eb30b07a8265e2db900c3c933a.png";
+import imgImage41 from "./612f95540528d0b39e47c7b7e0eec3ca7961d926.png";
+
+function CenturioDigitalAgencyNegative() {
+  return (
+    <div className="absolute h-[80px] left-[1680px] top-[73px] w-[180px]" data-name="centurio-digital-agency-negative 1">
+      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 180 80">
+        <g clipPath="url(#clip0_18_509)" id="centurio-digital-agency-negative 1">
+          <path d={svgPaths.p4ccc400} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector" />
+          <path d={svgPaths.pfbdf280} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_2" />
+          <path d={svgPaths.p1a8c5d00} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_3" />
+          <path d={svgPaths.p18359c80} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_4" />
+          <path d={svgPaths.p3ae3f180} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_5" />
+          <path d={svgPaths.p184e6f00} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_6" />
+          <path d={svgPaths.p2986c580} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_7" />
+          <path d={svgPaths.p3b23f700} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_8" />
+          <path d={svgPaths.p10087280} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_9" />
+          <path d={svgPaths.p74b3000} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_10" />
+          <path d={svgPaths.p1fecf300} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_11" />
+          <path d={svgPaths.p25db3500} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_12" />
+          <path d={svgPaths.p11f19200} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_13" />
+          <path d={svgPaths.p6a69200} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_14" />
+          <path d={svgPaths.p28c52600} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_15" />
+          <path d={svgPaths.p1bcf3e80} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_16" />
+          <path d={svgPaths.p2a05a80} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_17" />
+          <path d={svgPaths.p2946c100} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_18" />
+          <path d={svgPaths.p1a1943f0} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_19" />
+          <path d={svgPaths.p2cde8480} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_20" />
+          <path d={svgPaths.p26d86a00} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_21" />
+          <path d={svgPaths.p2da72900} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_22" />
+          <path d={svgPaths.p2150da00} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_23" />
+          <g id="Face">
+            <path d={svgPaths.p1d402780} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_24" />
+          </g>
+          <path d={svgPaths.p2916d800} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_25" />
+          <path d={svgPaths.p2b144900} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_26" />
+          <path d={svgPaths.p1f6399f0} fill="var(--fill-0, black)" fillOpacity="0.3" id="Vector_27" />
+        </g>
+        <defs>
+          <clipPath id="clip0_18_509">
+            <rect fill="white" height="80" width="180" />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+export default function Frame() {
+  return (
+    <div className="bg-white relative size-full" data-name="Frame">
+      <div className="-translate-y-1/2 [word-break:break-word] absolute flex flex-col font-['Inter:Regular',sans-serif] font-normal justify-center leading-[0] left-[100px] not-italic text-[20px] text-[rgba(0,0,0,0.3)] top-[calc(100%-88px)] tracking-[-0.44px] w-[382px]">
+        <p className="leading-[1.5]">Creative route</p>
+      </div>
+      <div className="-translate-y-1/2 [word-break:break-word] absolute flex flex-col font-['Inter:Regular',sans-serif] font-normal justify-center leading-[0] left-[calc(100%-150px)] not-italic text-[20px] text-[rgba(0,0,0,0.3)] top-[calc(100%-88px)] tracking-[-0.44px] w-[50px]">
+        <p className="leading-[1.5]">2026</p>
+      </div>
+      <CenturioDigitalAgencyNegative />
+      <div className="-translate-x-1/2 absolute h-0 left-1/2 top-[938px] w-[1764px]">
+        <div className="absolute inset-[-1px_0_0_0]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 1764 1">
+            <line id="Line 4" stroke="var(--stroke-0, black)" strokeOpacity="0.3" x2="1764" y1="0.5" y2="0.5" />
+          </svg>
+        </div>
+      </div>
+      <div className="[word-break:break-word] absolute font-['Plus_Jakarta_Sans:Bold',sans-serif] font-bold leading-[0] left-[1244px] text-[#0f3063] text-[60px] top-[245px] tracking-[-1.32px] w-[576px] whitespace-pre-wrap">
+        <p className="leading-none mb-0">01.</p>
+        <p className="leading-none mb-0">​</p>
+        <p className="leading-none">Arakii</p>
+      </div>
+      <p className="[word-break:break-word] absolute font-['Inter:Regular',sans-serif] font-normal leading-[0] left-[1244px] not-italic opacity-70 text-[24px] text-[rgba(0,0,0,0.7)] top-[517px] tracking-[-0.528px] w-[576px]">
+        <span className="leading-[28px]">{`Arakii is a Swedish contemporary fashion brand focused on `}</span>
+        <span className="font-['Inter:Bold',sans-serif] font-bold leading-[28px]">elevated essentials, minimalist design</span>
+        <span className="leading-[28px]">{`, and `}</span>
+        <span className="font-['Inter:Bold',sans-serif] font-bold leading-[28px]">high-quality materials</span>
+        <span className="leading-[28px]">{`. Rooted in Scandinavian aesthetics, the brand creates versatile pieces that emphasize `}</span>
+        <span className="font-['Inter:Bold',sans-serif] font-bold leading-[28px]">timeless style, functionality,</span>
+        <span className="leading-[28px]">{` and understated`}</span>
+        <span className="font-['Inter:Bold',sans-serif] font-bold leading-[28px]">{` luxury,`}</span>
+        <span className="leading-[28px]">{` appealing to consumers who value refined essentials over trend-driven fashion.`}</span>
+      </p>
+      <a className="[word-break:break-word] absolute block font-['Inter:Regular',sans-serif] font-normal leading-[0] left-[1244px] not-italic opacity-70 text-[24px] text-[rgba(0,0,0,0.7)] top-[746px] tracking-[-0.528px] w-[576px]" href="https://arakii.com/" target="_blank">
+        <p className="[text-underline-position:from-font] cursor-pointer decoration-from-font decoration-solid leading-[28px] underline">{`https://arakii.com/`}</p>
+      </a>
+      <div className="absolute h-[346px] left-[575px] top-[510px] w-[504px]" data-name="image 38">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgImage38} />
+      </div>
+      <div className="absolute h-[346px] left-[78px] top-[510px] w-[470px]" data-name="image 39">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgImage39} />
+      </div>
+      <div className="absolute h-[402px] left-[78px] top-[86px] w-[613px]" data-name="image 40">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgImage40} />
+      </div>
+      <div className="absolute h-[402px] left-[713px] top-[86px] w-[366px]" data-name="image 41">
+        <div className="absolute inset-0 overflow-hidden pointer-events-none">
+          <img alt="" className="absolute h-[113.26%] left-0 max-w-none top-[-1.77%] w-full" src={imgImage41} />
+        </div>
+      </div>
+    </div>
+  );
+}
